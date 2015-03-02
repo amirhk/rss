811e3222e66cb8790f23ae4c2f013d844cdc89d4
amirhk <amirhkarimi@gmail.com>
2015-03-01 23:18:22 -0500

    Removed dummy testers.. so I don't spam myself

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,10 @@ require 'open-uri'
 
 # if ENV["RAILS_ENV"] != :production
 
-  @u1 = User.create! :name => "Amir1", :email => "amirhkarimi+1@gmail.com", :notify_usd_to_cad => true, :notify_usd_to_eur => true, :notify_usd_to_irr => true
-  @u2 = User.create! :name => "Amir2", :email => "amirhkarimi+2@gmail.com", :notify_usd_to_cad => false, :notify_usd_to_eur => false, :notify_usd_to_irr => true
-  @u3 = User.create! :name => "Amir3", :email => "amirhkarimi+3@gmail.com", :notify_usd_to_cad => true, :notify_usd_to_eur => true, :notify_usd_to_irr => true
+  User.create! :name => "Amir-Hossein", :email => "amirhkarimi@gmail.com", :notify_usd_to_cad => true, :notify_usd_to_eur => true, :notify_usd_to_irr => true
+  # User.create! :name => "Amir1", :email => "amirhkarimi+1@gmail.com", :notify_usd_to_cad => true, :notify_usd_to_eur => true, :notify_usd_to_irr => true
+  # User.create! :name => "Amir2", :email => "amirhkarimi+2@gmail.com", :notify_usd_to_cad => false, :notify_usd_to_eur => false, :notify_usd_to_irr => true
+  # User.create! :name => "Amir3", :email => "amirhkarimi+3@gmail.com", :notify_usd_to_cad => true, :notify_usd_to_eur => true, :notify_usd_to_irr => true
 
   rate_CAD = JSON.parse(open("http://rate-exchange.appspot.com/currency?from=USD&to=CAD").read)["rate"]
   rate_EUR = JSON.parse(open("http://rate-exchange.appspot.com/currency?from=USD&to=EUR").read)["rate"]
