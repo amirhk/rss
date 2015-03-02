7eaa28d56c9451c1393aaf12bcb0fa4c0d5df985
amirhk <amirhkarimi@gmail.com>
2015-03-01 19:53:38 -0500

    Code from Peter for reading online USD to CAD rate via API

---
+++ lib/tasks/update_rate.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'open-uri'
+
+namespace :ball do
+  task :sack => :environment do
+
+    rate = JSON.parse(open("http://rate-exchange.appspot.com/currency?from=USD&to=CAD").read)["rate"]
+
+    qu = Quote.new(:value => rate)
+
+    if qu.save
+
+      qu.email("amir@amir.amir")
+
+    end
+
+    puts rate
+
+  end
+end
